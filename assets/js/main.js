b39bd1fcb0659c07cf4fb261eddd19776f8bcfff
Cfetecua <camilofetecua20111@gmail.com>
2025-04-15 18:45:27 -0500

    add a banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,4 +226,26 @@
   window.addEventListener('load', navmenuScrollspy);
   document.addEventListener('scroll', navmenuScrollspy);
 
+  // Show the modal on page load
+  document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("under-construction-modal");
+
+    // Show the modal
+    modal.style.display = "flex";
+  });
+
+  // Show the modal on page load
+  document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("under-construction-modal");
+    const closeModal = document.getElementById("close-modal");
+
+    // Show the modal
+    modal.style.display = "flex";
+
+    // Close the modal when the button is clicked
+    closeModal.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+  });
+
 })();
